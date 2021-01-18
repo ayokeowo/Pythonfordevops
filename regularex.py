4b6147395401f48e517fa4ffce957ec1537b666f
ayoo <ayoo@ayokeowo.com>
2021-01-18 23:08:39 +0000

    new

--- regularex.py
+++ regularex.py
@@ -1,3 +1,22 @@
 import re
 
+open_file = 'http_logs_for_analysis.txt'
+
+with open('http_logs_for_analysis.txt', 'r') as file:
+    x = file.readline()
+    y = '192.168.4.164' in x
+    print (type(x))
+    print (y)
+
+#z= re.search(r'192.168.4.164',x)
+
+#search only returns the first match found
+z =re.search(r'\w+\.\w+',x)
+print (z)
+
+#findall returns all matches
+
+q = re.findall(r'\w+\.\w+\.\w+\.\w+', x)
+
+print (q)
 
